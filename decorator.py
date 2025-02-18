3eeea1327195b922bf5a8bb192c4624c5572b1b3
Snuipr <Timagopher@gmail.com>
2025-02-19 00:16:24 +0300

    Update decorator.py

--- decorator.py
+++ decorator.py
@@ -10,17 +10,17 @@ def timer_func(func):
         print(f"Время работы: {times}")
         return [result, times]
     return wrapper
-
+    
 def reply(func):
     def wrapper(*args, repit=1, **kwargs):
         for i in range(repit):
            try:
                 responce = func(*args, **kwargs)
                 return responce
-           except requests.exceptions.ConnectionError:
-               print("Error connection, try again")
+           except:
+               print("Error, try again")
                continue
-        raise requests.exceptions.ConnectionError("Возможно нет подключения к интернету.")
+        raise Exception(f"Функция не заработала за {repit} попыток")
     return wrapper
 
 def check_time_func(func):
